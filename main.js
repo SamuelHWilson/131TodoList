00b71ce47d29b5d3f6be9667fb9cba00ae90f2a1
Unknown <swilsondev@outlook.com>
2017-12-04 09:10:06 -0600

    Prototyped menu.

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 app.on("ready", () => {
     var mainWindow = new BrowserWindow();
     mainWindow.loadURL(`file://${__dirname}/tasks.html`);
+    
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    mainWindow.setMenu(menu);
 
     /* Cleanup */
     mainWindow.on("closed", () => {
         mainWindow = null;
     });
 });
+
+const menuTemplate = [
+    {label: "Tasks", submenu: [
+        {label: "New Task", click: () => {}},
+        {label: "Clear All Tasks", click: () => {}},
+        {type: "separator"},
+        {label: "Load From File", click: () => {}}
+    ]},
+    {label: "Application", submenu: [
+        {label: "Close", role: "close"}
+    ]},
+    {label: "Developer", submenu: [
+        {label: "Toggle Developer Tools", role: "toggledevtools"},
+        {label: "Reload", role: "reload"}
+    ]}
+];
